42fa550059df04480622ecbd7dae5446e234df60
Andreia Sá <andreiapsa6@gmail.com>
2024-05-31 15:34:32 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -5,44 +5,83 @@ import TableHeader from "./components/TableHeader";
 import StudentCard from "./components/StudentCard";
 
 import studentsData from "./assets/students.json";
+import { useNavigate } from "react-router-dom";
 
 function App() {
   const [students, setStudents] = useState(studentsData);
+  const [fullName, setFullName] = useState("")
+  const [image, setImage] = useState("")
+  const [phone, setPhone] = useState("")
+  const [email, setEmail] = useState("")
+  const [program, setProgram] = useState("")
+  const [graduationYear, setGraduationYear] = useState(2023)
+  const [graduated, setGraduated] = useState(false)
+  
+  const nav = useNavigate()
 
+      function handleAddStudent(event){
+        event.preventDefault()
+        const newStudent = {fullName, image, phone, email, program, graduationYear, graduated, id: students.length + 1}
+        setStudents([...students, newStudent])
+        
+        setFullName("");
+        setImage("");
+        setPhone("");
+        setEmail("");
+        setProgram("");
+        setGraduationYear(2023);
+        setGraduated(false);
 
+      nav("/")
+      }
   return (
     <div className="App pt-20">
       <Navbar />
 
       {/* FORM */}
-      <form>
+      <form onSubmit={handleAddStudent}>
         <span>Add a Student</span>
         <div>
           <label>
             Full Name
-            <input name="fullName" type="text" placeholder="Full Name" />
+            <input name="fullName" type="text" placeholder="Full Name" value={fullName}
+            onChange={(event)=>{
+              setFullName(event.target.value)
+            }} />
           </label>
 
           <label>
             Profile Image
-            <input name="image" type="url" placeholder="Profile Image" />
+            <input name="image" type="url" placeholder="Profile Image" value={image}
+            onChange={(event)=>{
+              setImage(event.target.value)
+            }} />
           </label>
 
           <label>
             Phone
-            <input name="phone" type="tel" placeholder="Phone" />
+            <input name="phone" type="tel" placeholder="Phone" value={phone}
+            onChange={(event)=>{
+              setPhone(event.target.value)
+            }} />
           </label>
 
           <label>
             Email
-            <input name="email" type="email" placeholder="Email" />
+            <input name="email" type="email" placeholder="Email" value={email}
+            onChange={(event)=>{
+              setEmail(event.target.value)
+            }} />
           </label>
         </div>
 
         <div>
           <label>
             Program
-            <select name="program">
+            <select name="program" value={program} 
+              onChange={(event)=>{
+              setProgram(event.target.value)
+            }}>
               <option value="">-- None --</option>
               <option value="Web Dev">Web Dev</option>
               <option value="UXUI">UXUI</option>
@@ -56,16 +95,23 @@ function App() {
               name="graduationYear"
               type="number"
               placeholder="Graduation Year"
+              value={graduationYear}
               minLength={4}
               maxLength={4}
               min={2023}
               max={2030}
+              onChange={(event)=>{
+                setGraduationYear(event.target.value)
+              }}
             />
           </label>
 
           <label>
             Graduated
-            <input name="graduated" type="checkbox" />
+            <input name="graduated" type="checkbox" checked={graduated}
+           onChange={(event)=>{
+            setGraduated(event.target.checked)
+          }} />
           </label>
 
           <button type="submit">Add Student</button>
